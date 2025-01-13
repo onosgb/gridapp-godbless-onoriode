0f3d00bd00f7cbfb1a7daffeebec7e1d341642b3
onosgb <onosgb@gmail.com>
2025-01-13 12:12:18 +0100

    active routed updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gridapp';
+  route = signal('/');
+
+  activeRoute = computed(() => {
+    return this.route();
+  });
+
+  routes = signal([
+    {
+      url: '/',
+      label: 'Grid1',
+    },
+    {
+      url: '/sorting',
+      label: 'Grid2',
+    },
+    {
+      url: '/filtering',
+      label: 'Grid3',
+    },
+    {
+      url: '/editing',
+      label: 'Grid4',
+    },
+  ]);
+  navigateTo(route: string) {
+    this.route.set(route);
+  }
 }
